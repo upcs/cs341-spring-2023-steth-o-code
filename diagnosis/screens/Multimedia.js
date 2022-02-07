95654f61c8afab9a9322fa93babd864d42813a35
millers23 <millers23@up.edu>
2022-02-06 20:50:51 -0800

    WIP continuation

--- diagnosis/screens/Multimedia.js
+++ diagnosis/screens/Multimedia.js
@@ -1,6 +1,7 @@
 import React, {useState}  from 'react';
 import { View, Text, Switch, Stylesheet } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import RadioGroup from 'react-native-radio-button-group';
 
 //formik
 import { Formik } from 'formik';
@@ -27,28 +28,62 @@ import {Octicons, Ionicons, Fontisto } from '@expo/vector-icons';
 
 const {company, placeholder, textInputBackground} = Colors;
 
+var radiogroup_options = [
+    {id: 0, label: 'Normal' },
+    {id: 1, label: 'Annotated' },
+    {id: 2, label: 'Narrated' },
+];
+
 const Multimedia = () => {
     let num_patients = 1;
-    const VIMEO_ID = '179859217';
-    const PROGNOSIS = "PLACEHOLDER"
-    const DESCRIPTION = "PLACEHOLDER"
-    const BLOCK1 = "PLACEHOLDER";
-    const BLOCK2 = "PLACEHOLDER";
-    const BLOCK3 = "PLACEHOLDER";
-    const BLOCK4 = "PLACEHOLDER";
-    const BLOCK5 = "PLACEHOLDER";
+    let prognosis = "PLACEHOLDER"
+    let description = "PLACEHOLDER"
+    let block1 = "PLACEHOLDER";
+    let ids1 = [472325227, 472325047, 505014368];//Normal, Annotated, Narrated
+    let block2 = "PLACEHOLDER";
+    let ids2 = [0, 0, 0];//Normal, Annotated, Narrated
+    let block3 = "PLACEHOLDER";
+    let ids3 = [0, 0, 0];//Normal, Annotated, Narrated
+    let block4 = "PLACEHOLDER";
+    let ids4 = [0, 0, 0];//Normal, Annotated, Narrated
+    let block5 = "PLACEHOLDER";
+    let ids5 = [0, 0, 0];//Normal, Annotated, Narrated
 
     const [isEnabled, setIsEnabled] = useState(false);
     const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
+    //for (let i = 0; i < num_patients; i++) {
+    // maybe I could do this with a custom datatype?
+    //}
+
+    var RadioGroupProject = React.createClass({
+        getInitialState: function() {
+            return {
+                selectedOption: 0,
+            }
+        },
+        render: function() {
+            return (
+                <View>
+                    <RadioGroup
+                        options={radiogroup_options}
+                        onChange={(option) => this.setState({selectedOption: option})}
+                    />
+                </View>
+            );
+        }
+    });
 
     return (
         <StyledContainer>
             <StatusBar style="dark" />
             <InnerContainer>
+                {/*Page head and logo*/}
                 <PageLogo resizeMode="contain" source={require('./../assets/cropped-logo_new-1.png')} />
-                <PageTitle>{ PROGNOSIS }</PageTitle>
-                <Text style={styles.baseText}>{ DESCRIPTION }</Text>
-                
+                <PageTitle>{ prognosis }</PageTitle>
+                {/*Basic description*/}
+                <Text style={styles.baseText}>{ description }</Text>
+                {/*Available offline switch*/}
                 <View style={styles.container}>
                     <Text>{enabled ? "Make page available offline" : "Available offline"}</Text>
                     <Switch
@@ -59,10 +94,21 @@ const Multimedia = () => {
                         value={isEnabled}
                     />
                 </View>
+                {/*End of universal section*/}
 
+                {/*Patient 1 section*/}
+                <Text style={styles.header}>Patient 1</Text>
+                {/*Block text for patient 1*/}
+                <Text style={styles.baseText}>{ block1 }</Text>
+                {/*Video type selection*/}
+                <RadioGroup
+                    horizontal
+                    options={radiogroup_options}
+                />
+                {/*Video itself, should change depending on selected radio button*/}
                 <Vimeo
                     ref='video'
-                    videoId={VIMEO_ID}
+                    videoId={ids1[options]}
                     onReady={ () => console.log('Vimeo is ready') }
                     onPlay={ () => console.log('Vimeo is playing') }
                     onPlayProgress={ data => console.log('Vimeo progress data:', data) }
