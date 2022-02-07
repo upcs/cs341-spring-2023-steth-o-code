ed59f2909be76c0e2a138f341def592d8380ec29
millers23 <millers23@up.edu>
2022-02-07 15:45:50 -0800

    why are radio buttons so hard

--- diagnosis/screens/Multimedia.js
+++ diagnosis/screens/Multimedia.js
@@ -1,7 +1,8 @@
 import React, {useState}  from 'react';
 import { View, Text, Switch, Stylesheet } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import RadioGroup from 'react-native-radio-button-group';
+import {RadioGroup, Radio} from '@adobe/react-spectrum'
+
 
 //formik
 import { Formik } from 'formik';
@@ -28,13 +29,8 @@ import {Octicons, Ionicons, Fontisto } from '@expo/vector-icons';
 
 const {company, placeholder, textInputBackground} = Colors;
 
-var radiogroup_options = [
-    {id: 0, label: 'Normal' },
-    {id: 1, label: 'Annotated' },
-    {id: 2, label: 'Narrated' },
-];
-
 const Multimedia = () => {
+    let [selected, setSelected] = React.useState('');
     let num_patients = 1;
     let prognosis = "PLACEHOLDER"
     let description = "PLACEHOLDER"
@@ -49,45 +45,20 @@ const Multimedia = () => {
     let block5 = "PLACEHOLDER";
     let ids5 = [0, 0, 0];//Normal, Annotated, Narrated
 
-    const [isEnabled, setIsEnabled] = useState(false);
-    const toggleSwitch = () => setIsEnabled(previousState => !previousState);
-
-    //for (let i = 0; i < num_patients; i++) {
-    // maybe I could do this with a custom datatype?
-    //}
-
-    var RadioGroupProject = React.createClass({
-        getInitialState: function() {
-            return {
-                selectedOption: 0,
-            }
-        },
-        render: function() {
-            return (
-                <View>
-                    <RadioGroup
-                        options={radiogroup_options}
-                        onChange={(option) => this.setState({selectedOption: option})}
-                    />
-                </View>
-            );
-        }
-    });
-
     return (
         <StyledContainer>
             <StatusBar style="dark" />
             <InnerContainer>
                 {/*Page head and logo*/}
                 <PageLogo resizeMode="contain" source={require('./../assets/cropped-logo_new-1.png')} />
-                <PageTitle>{ prognosis }</PageTitle>
+                <PageTitle>{prognosis}</PageTitle>
                 {/*Basic description*/}
-                <Text style={styles.baseText}>{ description }</Text>
+                <Text style={styles.baseText}>{description}</Text>
                 {/*Available offline switch*/}
                 <View style={styles.container}>
                     <Text>{enabled ? "Make page available offline" : "Available offline"}</Text>
                     <Switch
-                        trackColor={{ false: "#767577", true: "#81b0ff" }}
+                        trackColor={{false: "#767577", true: "#81b0ff"}}
                         thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
                         ios_backgroundColor="#3e3e3e"
                         onValueChange={toggleSwitch}
@@ -99,12 +70,7 @@ const Multimedia = () => {
                 {/*Patient 1 section*/}
                 <Text style={styles.header}>Patient 1</Text>
                 {/*Block text for patient 1*/}
-                <Text style={styles.baseText}>{ block1 }</Text>
-                {/*Video type selection*/}
-                <RadioGroup
-                    horizontal
-                    options={ radiogroup_options }
-                />
+                <Text style={styles.baseText}>{block1}</Text>
                 {/*Video itself, should change depending on selected radio button*/}
                 <Vimeo
                     ref='video'
@@ -114,6 +80,16 @@ const Multimedia = () => {
                     onPlayProgress={ data => console.log('Vimeo progress data:', data) }
                     onFinish={ () => console.log('Vimeo is finished') }
                 />
+                {/*Video type selection*/}
+                <RadioGroup
+                    label="Video Options"
+                    defaultValue="0"
+                    value={selected}
+                    onChange={setSelected}>
+                    <Radio value="0">Normal</Radio>
+                    <Radio value="1">Annotated</Radio>
+                    <Radio value="2">Narrated</Radio>
+                </RadioGroup>
             </InnerContainer>
         </StyledContainer>
     );
