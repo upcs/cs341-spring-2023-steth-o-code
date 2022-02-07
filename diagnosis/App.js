6f1ce4ff1b102a3a7038efc1598328edf1feb17c
Devam Patel <Devam29@gmail.com>
2022-02-06 22:28:50 -0800

    added explore menu along with some buttons.

--- diagnosis/App.js
+++ diagnosis/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 //screens import
 import Login from './screens/Login';
+import MainMenu from './screens/MainMenu';
 import Signup from './screens/Signup';
 
 export default function App() {
-  return <Signup />;
+  return <MainMenu />;
 }
--- diagnosis/screens/MainMenu.js
+++ diagnosis/screens/MainMenu.js
@@ -1,22 +1,57 @@
 import React from 'react';
-import { Text, View, StyleSheet, Image } from 'react-native';
-import { StyledContainer } from '../components/styles';
-import DropdownMenu from 'react-native-dropdown-picker';
-import Slideshow from 'react-native-image-slider-show';
+import { Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
 var time = new Date().getHours();
 
+var button1Cliked = () => {
+    alert("Button 1 clicked");
+}
+
+var button2Cliked = () => {
+    alert("Button 2 clicked");
+}
+
+var button3Cliked = () => {
+    alert("Button 3 clicked");
+}
+
+var button4Cliked = () => {
+    alert("Button 4 clicked");
+}
+
 const MainMenu = () => (
+
+    
         <View style={styles.mainContainer}> 
             <View style={styles.headerContainer}>   
                 <Image style={styles.PDXLogo} source={require('diagnosis/assets/simplePDXLogo.png')}></Image>
                 <Image style={styles.accountIcon} source={require('diagnosis/assets/accountIcon.png')}></Image>
             </View>
             
-            <Image style={styles.stethoscopeImage} source={require('diagnosis/assets/stethoscope.jpeg')} blurRadius={1.5}></Image>
+            <Image style={styles.stethoscopeImage} source={require('diagnosis/assets/stethoscope.jpeg')} blurRadius={1}></Image>
             <Text style={styles.welcomeText}>Good Morning, {"\n"}
                 Guest
             </Text>
+
+            <View style={styles.quickLinksView}>
+                <Text style={styles.exploreText}>Explore:</Text>
+
+                <TouchableOpacity style={styles.quickLinkButtons} onPress={button1Cliked}> 
+                    <Text style={styles.buttonText}>Knowledge Test</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.quickLinkButtons} onPress={button2Cliked}> 
+                    <Text style={styles.buttonText}>Topic of the Day</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.quickLinkButtons} onPress={button3Cliked}> 
+                    <Text style={styles.buttonText}>Community</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.quickLinkButtons} onPress={button4Cliked}> 
+                    <Text style={styles.buttonText}>About Us</Text>
+                </TouchableOpacity>
+            </View>
         </View>
 );
 
@@ -36,7 +71,7 @@ const styles = StyleSheet.create({
         height: 100,
         justifyContent: "center",
         alignContent: "center",
-        width: 375,
+        width: '100%',
     },
 
     PDXLogo: {
@@ -65,13 +100,6 @@ const styles = StyleSheet.create({
         top: 38
     },
 
-    slideShow: {
-        // resizeMode: "center",
-        // position: "relative",
-        // width: 375,
-        // top: -106,
-    },
-
     welcomeText: {
         position: "absolute",
         color: "#E63610",
@@ -84,6 +112,52 @@ const styles = StyleSheet.create({
         textShadowOffset:{width: 5, height: 5},
         textShadowRadius:10,
         
+    },
+    
+    quickLinksView: {
+        top: 65,
+        backgroundColor: "#f0f0f0",
+        justifyContent: 'center',
+        position: "relative",
+        marginTop: 280,
+        height: 370,
+        width: 350,
+        borderRadius: 15,
+        borderColor: 'red',
+        alignContent: 'center'
+
+    },
+
+    exploreText: {
+        fontSize: 45,
+        marginBottom: 30,
+        fontFamily: "Gill Sans",
+        position: "relative",
+        marginLeft: 110
+    },
+
+    magGlassImg: {
+        resizeMode: "contain",
+        width: '15%',
+        position: 'relative'
+    },
+
+    quickLinkButtons: {
+        marginTop: 9,
+        alignItems: 'center',
+        marginLeft: 50,
+        width: '75%',
+        paddingVertical: 10,
+        backgroundColor: 'red',
+        borderRadius: 30,
+        borderColor: 'black'
+
+    },
+
+    buttonText: {
+        fontSize: 25,
+        fontFamily: "Gill Sans",
+        fontWeight: "bold"
     }
 });
     
