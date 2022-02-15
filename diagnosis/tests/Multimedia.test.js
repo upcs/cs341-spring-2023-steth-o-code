12fb4f60115a5acc138c2e3df1947bd245ac9c21
millers23 <millers23@up.edu>
2022-02-15 10:51:07 -0800

    struggling

--- diagnosis/tests/Multimedia.test.js
+++ diagnosis/tests/Multimedia.test.js
@@ -4,17 +4,17 @@ import React from 'react'
 import { render, fireEvent, cleanup, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 
-const multi = require('/src/screens/Multimedia');
+const multi = require('./src/screens/Multimedia');
 
 test('test Switch', () => {
-  render(<Multimedia />);
+  render(<multi />);
   expect(isEnabled).toBe(false);//default
   userEvent.click(screen.getByTestID('switch'))
   expect(isEnabled).toBe(true);//switch if clicked
 });
 
 test('test Button Group', () => {
-  render(<Multimedia />);
+  render(<multi />);
   expect(selectedIndex).toBe(0);//default
   userEvent.click(screen.getByText('Annotated'))
   expect(selectedIndex).toBe(1);//option 1
@@ -23,7 +23,7 @@ test('test Button Group', () => {
 });
 
 test('test Vimeo', () => {
-  render(<Multimedia />);
+  render(<multi />);
   expect(screen.getByTestID('video')).not.toBeNull();//video exists
   //write more test here
 })
