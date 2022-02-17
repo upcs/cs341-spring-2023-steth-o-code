b687bba8854bfc50f45785e23853223c41952679
martinea23 <martinea23@up.edu>
2022-02-17 09:56:03 -0800

    test

--- diagnosis/App.js
+++ diagnosis/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
 
 //screens import
-import Login from './screens/Login';
-import Multimedia from './screens/Multimedia';
-import Signup from './screens/Signup';
-import MainMenu from './screens/MainMenu';
 import StackNavigator from './navigators/StackNavigator';
 
 export default function App() {
