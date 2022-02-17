0d80d109d22ee383527ad3c01ad1cc9ec67669b3
tamsendean <deant23@up.edu>
2022-02-17 09:51:05 -0800

    topic

--- diagnosis/App.js
+++ diagnosis/App.js
@@ -1,12 +1,7 @@
 import React from 'react';
 
 //screens import
-import Login from './screens/Login';
-import Multimedia from './screens/Multimedia';
-import Signup from './screens/Signup';
-import MainMenu from './screens/MainMenu';
 import StackNavigator from './navigators/StackNavigator';
-import Topics from './screens/Topics';
 
 export default function App() {
   return <StackNavigator />;
