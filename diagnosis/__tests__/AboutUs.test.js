4feadc4da0c3e9524e05d34ffe9ed66f5d670482
AshAnand34 <74620246+ananda23-cs@users.noreply.github.com>
2022-02-17 11:48:55 -0800

    fixed jest issue

--- diagnosis/__tests__/AboutUs.test.js
+++ diagnosis/__tests__/AboutUs.test.js
@@ -7,14 +7,14 @@ import { ITEM_WIDTH } from '../components/CarouselCardItem';
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
 
 describe('<AboutUs />', () => {
-    test("it should render correctly", () => {
+    test("it should render correctly", async () => {
         renderer.create(<AboutUs />);
     });
-    test("user can scroll through text", () => {
+    test("user can scroll through text", async () => {
         const { getByTestId } = render(<AboutUs />);
         fireEvent.scroll(getByTestId('text-scroll'), { target: { scrollY: 300 }});
     });
-    test("carousel should work", () => {
+    test("carousel should work", async () => {
         const { getByTestId } = render(<AboutUs />);
         const carousel = getByTestId('carousel');
         fireEvent.scroll(carousel, { nativeEvent: { contentOffset: { scrollX: ITEM_WIDTH }, contentSize: { width: ITEM_WIDTH, height: 300 }}})
