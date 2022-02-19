d8108ce3f60dc009aec682cd796c0bb7afb72093
AshAnand34 <74620246+ananda23-cs@users.noreply.github.com>
2022-02-18 20:01:40 -0800

    started making team info page

--- diagnosis/screens/TeamInfo.js
+++ diagnosis/screens/TeamInfo.js
@@ -1,21 +1,57 @@
 import React from 'react';
-import { Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { Text, View, StyleSheet, Image, ScrollView, Dimensions } from 'react-native';
 
 const TeamInfo = () => (
-    <View style={styles.mainContainer}>
-        <Text style={styles.text}> PAGE FOR INFO ABOUT THE TEAM</Text>
+    <View style={{flexGrow: 1}}>
+        <ScrollView style={{maxHeight: Dimensions.get('window').height}} contentContainerStyle={styles.mainContainer}>
+            <Text style={styles.title}>Meet the Team</Text>
+            <View>
+                <Image
+                    style={{ width: 200, height: 200}} 
+                    source={require('./../assets/Andre-314x314.png')}
+                />
+                <Text style={styles.person}>André M. Mansoor, M.D.</Text>
+            </View>
+            <View>
+                <Image
+                   style={{ width: 200, height: 200}} 
+                    source={require('./../assets/Sullivan.jpeg')}
+                />
+                <Text style={styles.person}>Peter Sullivan, M.D.</Text>
+            </View>
+            <View>
+                <Image
+                    style={{ width: 200, height: 200}} 
+                    source={require('./../assets/jgn.jpg')}
+                />
+                <Text style={styles.person}>Joseph G. Nugent, M.H.S.</Text>
+            </View>
+            <View>
+                <Image
+                    style={{ width: 200, height: 200}} 
+                    source={require('./../assets/ishita-sharma.jpeg')}
+                />
+                <Text style={styles.person}>Ishita Sharma, M.B.B.S.</Text>
+            </View>
+        </ScrollView>
     </View>
 );
 
 const styles = StyleSheet.create({
     mainContainer: {
-        backgroundColor: 'white',
         flex: 1,
-        justifyContent: "center",
         alignItems: "center",
+        padding: 25,
         backgroundColor: "#BFAA8C",
     },
-
+    title: {
+        fontSize: 30,
+        fontWeight: "bold",
+        color: "red",
+    },
+    person: {
+        fontWeight: "bold"
+    }
 });
 
 export default TeamInfo;
