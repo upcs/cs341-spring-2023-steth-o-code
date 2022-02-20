3bb90a2fbc6dbb257ca8f8663f0273119375f121
AshAnand34 <74620246+ananda23-cs@users.noreply.github.com>
2022-02-19 19:20:26 -0800

    created test file for team info page

---
+++ diagnosis/__tests__/TeamInfo.test.js
@@ -0,0 +1,18 @@
+import { fireEvent, render } from '@testing-library/react-native';
+import React from 'react';
+import renderer from 'react-test-renderer';
+import TeamInfo from '../screens/TeamInfo';
+
+describe('<TeamInfo />', function() {
+    test('renders correctly', function() {
+        renderer.create(<TeamInfo />);
+    });
+    test('social icon works', function() {
+        const press = jest.fn();
+        const {getByTestId} = render(<TeamInfo onPress={{ press }} />);
+        const twitter = getByTestId('twitter');
+        expect(twitter).toBeTruthy();
+        fireEvent.press(twitter);
+        expect(press).toHaveBeenCalledTimes(1);
+    })
+});
