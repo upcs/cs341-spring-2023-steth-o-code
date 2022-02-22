dc2f6d898802877c71e6b4925d33d36383ec2de8
tamsendean <deant23@up.edu>
2022-02-22 10:00:29 -0800

    test for main menu

---
+++ diagnosis/tests/test1.js
@@ -0,0 +1,44 @@
+/**
+ * Test for Main Menu
+ */
+
+ import React from 'react';
+ import {ActivityIndicator, FlatList, Text, TextInput} from 'react-native';
+ import renderer from 'react-test-renderer';
+ import MainMenu from '../screens/MainMenu';
+ 
+ jest.setTimeout(15000);
+ 
+ it('renders correctly', () => {
+   const tree = renderer.create(<MainMenu />).toJSON();
+   expect(tree).toMatchSnapshot();
+ });
+ 
+ // These serve as integration tests for the jest-react-native preset.
+ it('renders the ActivityIndicator component', () => {
+   const tree = renderer
+     .create(<ActivityIndicator animating={true} size="small" />)
+     .toJSON();
+   expect(tree).toMatchSnapshot();
+ });
+ 
+ it('renders the TextInput component', () => {
+   const tree = renderer
+     .create(<TextInput autoCorrect={false} value="apple banana kiwi" />)
+     .toJSON();
+   expect(tree).toMatchSnapshot();
+ });
+ 
+ it('renders the FlatList component', () => {
+   const tree = renderer
+     .create(
+       <FlatList
+         data={['apple', 'banana', 'kiwi']}
+         keyExtractor={item => item}
+         renderItem={({item}) => <Text>{item}</Text>}
+       />,
+     )
+     .toJSON();
+   expect(tree).toMatchSnapshot();
+ });
+ 
