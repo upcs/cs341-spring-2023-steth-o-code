80e769ca317ef162e970dc36f1d37aaabe932b34
tamsendean <deant23@up.edu>
2022-02-22 12:00:18 -0800

    edit topics

--- diagnosis/screens/topics.js
+++ diagnosis/screens/topics.js
@@ -1,57 +1,116 @@
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, ScrollView, StyledContainer, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Dimensions, Image, StyledContainer, TouchableOpacity } from 'react-native';
+import {Ionicons} from '@expo/vector-icons';
 
 const Topics = ({navigation}) => (
-    <ScrollView contentContainerStyle={styles.StyledContainer}>
-    <Text style={styles.Text}>Topics</Text>
-    <View style={styles.buttonView}>
-    <TouchableOpacity style={styles.buttons} onPress={() => navigation.navigate("DefaultTopic")}> 
-    <Text style={styles.buttonsText}>Cardiology</Text>    
-    </TouchableOpacity>
-    <TouchableOpacity style={styles.buttons} onPress={() => navigation.navigate("DefaultTopic")}> 
-    <Text style={styles.buttonsText}>Endocrinology</Text>    
-    </TouchableOpacity>
-    <TouchableOpacity style={styles.buttons} onPress={() => navigation.navigate("DefaultTopic")}> 
-    <Text style={styles.buttonsText}>Nails</Text>    
-    </TouchableOpacity>
-    <TouchableOpacity style={styles.buttons} onPress={() => navigation.navigate("DefaultTopic")}> 
-    <Text style={styles.buttonsText}>Neurology</Text>    
-    </TouchableOpacity>
-    <TouchableOpacity style={styles.buttons} onPress={() => navigation.navigate("DefaultTopic")}> 
-    <Text style={styles.buttonsText}>Rheumatology</Text>    
-    </TouchableOpacity>
-    <TouchableOpacity style={styles.buttons} onPress={() => navigation.navigate("DefaultTopic")}> 
-    <Text style={styles.buttonsText}>Pulmonology</Text>    
-    </TouchableOpacity>
-    <TouchableOpacity style={styles.buttons} onPress={() => navigation.navigate("DefaultTopic")}> 
-    <Text style={styles.buttonsText}>Gastroenterology</Text>    
-    </TouchableOpacity>
-    <TouchableOpacity style={styles.buttons} onPress={() => navigation.navigate("DefaultTopic")}> 
-    <Text style={styles.buttonsText}>Miscellaneous</Text>    
-    </TouchableOpacity>
-    </View>
-    </ScrollView>
-    );
+  <View style={styles.mainContainer}> 
+  <View style={styles.headerContainer}>
+  <View>
+  <Ionicons style={styles.MainMenu} name='menu' size={35}/>
+  </View>
+  <Image style={styles.PDXLogo} source={require('diagnosis/assets/simplePDXLogo.png')}></Image>
+  <Image style={styles.accountIcon} source={require('diagnosis/assets/accountIcon.png')}></Image>
+  <Text style={styles.Text}>Topics</Text>
+  </View>
+  
+  
+  <ScrollView contentContainerStyle={styles.StyledContainer}>
+  
+  <View style={styles.buttonView}>
+  <TouchableOpacity style={styles.buttons} onPress={() => navigation.navigate("DefaultTopic")}> 
+  <Text style={styles.buttonsText}>Cardiology</Text>    
+  </TouchableOpacity>
+  <TouchableOpacity style={styles.buttons} onPress={() => navigation.navigate("DefaultTopic")}> 
+  <Text style={styles.buttonsText}>Endocrinology</Text>    
+  </TouchableOpacity>
+  <TouchableOpacity style={styles.buttons} onPress={() => navigation.navigate("DefaultTopic")}> 
+  <Text style={styles.buttonsText}>Nails</Text>    
+  </TouchableOpacity>
+  <TouchableOpacity style={styles.buttons} onPress={() => navigation.navigate("DefaultTopic")}> 
+  <Text style={styles.buttonsText}>Neurology</Text>    
+  </TouchableOpacity>
+  <TouchableOpacity style={styles.buttons} onPress={() => navigation.navigate("DefaultTopic")}> 
+  <Text style={styles.buttonsText}>Rheumatology</Text>    
+  </TouchableOpacity>
+  <TouchableOpacity style={styles.buttons} onPress={() => navigation.navigate("DefaultTopic")}> 
+  <Text style={styles.buttonsText}>Pulmonology</Text>    
+  </TouchableOpacity>
+  <TouchableOpacity style={styles.buttons} onPress={() => navigation.navigate("DefaultTopic")}> 
+  <Text style={styles.buttonsText}>Gastroenterology</Text>    
+  </TouchableOpacity>
+  <TouchableOpacity style={styles.buttons} onPress={() => navigation.navigate("DefaultTopic")}> 
+  <Text style={styles.buttonsText}>Miscellaneous</Text>    
+  </TouchableOpacity>
+  </View>
+  </ScrollView>
+  </View>
+  );
   
   const styles = StyleSheet.create({
+    mainContainer: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "#BFAA8C",
+      height: Dimensions.get('window').height,
+      width: Dimensions.get('window').width
+    },
+    headerContainer: {
+      backgroundColor: "#ffffff",
+      position: "absolute",
+      top: 0,
+      height: 100,
+      justifyContent: "center",
+      alignContent: "center",
+      width: '100%',
+  },
+  MainMenu:{
+      top: 10,
+      left: 20
+  },
+  PDXLogo: {
+      position:"absolute",
+      resizeMode: "contain",
+      width: 200,
+      top: -60,
+      left: 90,
+      flex: 1,
+  },
+
+  accountIcon: {
+      position: "absolute",
+      resizeMode: "contain",
+      width: 50,
+      height: 50,
+      right: 15,
+      top: 40
+  },
     StyledContainer: {
       backgroundColor: 'white',
       flex: 1,
       justifyContent: "center",
       alignItems: "center",
       backgroundColor: "white",
+      marginTop: 300,
+      width: Dimensions.get('window').width - 50
     },
     scrollView: {
       backgroundColor: 'white',
       marginHorizontal: 20,
+      marginTop: 2
     },
     Text: {
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      fontSize: 30,
-      marginTop: 10,
-      marginBottom: 10,
+        position: "absolute",
+        color: "#E63610",
+        fontSize: 60,
+        top: 120,
+        fontWeight: 'bold',
+        textAlign: "center",
+        fontFamily: "Gill Sans",
+        textShadowColor:'#585858',
+        textShadowOffset:{width: 5, height: 5},
+        textShadowRadius:10,
+        alignContent: 'center'
     },
     buttons: {
       marginTop: 15,
