7d005be81d24c0479afe3fb3223bd533d003dd50
Devam Patel <Devam29@gmail.com>
2022-03-10 12:44:51 -0800

    data loaded from db

--- diagnosis/screens/KnowledgeTest.js
+++ diagnosis/screens/KnowledgeTest.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { Text, View, StyleSheet, Image, TouchableOpacity, Linking } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
 import WebView from 'react-native-webview';
 
@@ -10,8 +10,9 @@ const KnowledgeTest = () => (
     // </View>
     
     <WebView 
-    javaScriptEnabled={true} 
-    source={{uri: 'https://up.physicaldiagnosispdx.com/up/app-content/database_connect_jgn.php'}}/>
+    javaScriptEnabled={true}
+    source={{uri: 'https://up.physicaldiagnosispdx.com/up/app-content/database_connect.php'}}
+    />
 );
 
 const styles = StyleSheet.create({
