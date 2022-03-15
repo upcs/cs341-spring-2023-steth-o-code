f13a09734f7db24af82cd38c66d310e0318b9443
millers23 <millers23@up.edu>
2022-03-14 20:15:22 -0700

    hey, it's not PLACEHOLDER

--- diagnosis/screens/Community.js
+++ diagnosis/screens/Community.js
@@ -3,7 +3,14 @@ import { Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
 const Community = () => (
     <View style={styles.mainContainer}>
-        <Text style={styles.text}> PAGE FOR COMMUNITY</Text>
+        <View style={styles.headerContainer}>
+                <View>
+                    <Ionicons style={styles.Community} name='community' size={35}/>
+                </View>
+                <Image style={styles.PDXLogo} source={require('diagnosis/assets/simplePDXLogo.png')}></Image>
+                <Image style={styles.accountIcon} source={require('diagnosis/assets/accountIcon.png')}></Image>
+                <Text style={styles.text}> Message Board </Text>
+        </View>
     </View>
 );
 
