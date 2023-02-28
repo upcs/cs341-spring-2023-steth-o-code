d8ecd7cd6f921b7212016e0128223481738dfd40
Emma Leann Kelly <103087185+kellye24@users.noreply.github.com>
2023-02-28 11:46:06 -0800

    works
    working version on this device

--- diagnosis/components/CarouselCardItem.js
+++ diagnosis/components/CarouselCardItem.js
@@ -8,7 +8,7 @@ const CarouselCardItem = ({ item, index }) => {
     return (
         <View style={styles.container} key={index}>
             <Image
-                source={{ uri: item }} 
+                source={{ uri: item }}
                 style={styles.image}
             />
         </View>
